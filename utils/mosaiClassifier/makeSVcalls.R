4a36222e3f8bede8902c0e6f20b5200e947f0ed1
Tobias Marschall <tobias.marschall@0ohm.net>
2018-07-03 13:57:19 +0200

    First (hacky) attempt on population priors, already cleans up a good fraction of wrong low VAF calls

--- utils/mosaiClassifier/makeSVcalls.R
+++ utils/mosaiClassifier/makeSVcalls.R
@@ -56,6 +56,59 @@ makeSVCallSimple <- function(probs, llr_thr = 1) {
   return(probs[sv_call_name != "ref_hom" & llr_to_ref > llr_thr])
 }
 
+
+makeSVCallWithPopPriors <- function(probs, llr_thr = 1) {
+
+  assert_that(is.data.table(probs),
+              "sample" %in% colnames(probs),
+              "cell"   %in% colnames(probs),
+              "chrom"  %in% colnames(probs),
+              "start"  %in% colnames(probs),
+              "end"    %in% colnames(probs),
+              "haplo_name" %in% colnames(probs),
+              "haplotype"  %in% colnames(probs),
+              "nb_hap_ll"  %in% colnames(probs)) %>% invisible
+
+  # make sure post-processing was done beforehands
+  assert_that("nb_hap_pp" %in% colnames(probs)) %>% invisible
+  setkey(probs, chrom, start, end, sample, cell)
+
+  probs[, pop.prior := sum(nb_hap_ll) , by = .(chrom, start, end, sample, haplotype)]
+  probs[, pop.prior := pop.prior/sum(pop.prior) , by = .(chrom, start, end, sample, cell)]
+  probs[, nb_hap_pp := nb_hap_pp*pop.prior]
+
+  # annotate the ref_hom posterior probability per segment / cell
+  probs[,
+        ref_hom_pp := .SD[haplo_name == "ref_hom", nb_hap_pp],
+        by = .(chrom, start, end, sample, cell)]
+
+  # order the different haplotype states based on their posterior prob. (nb_hap_pp)
+  # and keep only the two most likely states
+  probs[,
+        rank := frank(-nb_hap_pp, ties.method = "random"), # random should never allow ties to get the same rank!
+        by = .(chrom, start, end, sample, cell)]
+  probs <- probs[rank <= 2]
+  # Sort by rank within each group
+  setkey(probs, chrom, start, end, sample, cell, rank)
+
+  # select first and 2nd SV call
+  probs[, `:=`(sv_call_name          = haplo_name[rank==1],
+               sv_call_haplotype     = haplotype[rank==1],
+               sv_call_name_2nd      = haplo_name[rank==2],
+               sv_call_haplotype_2nd = haplotype[rank==2],
+               llr_to_ref            = log(nb_hap_pp[rank==1]) - log(ref_hom_pp[rank==1]),
+               llr_to_2nd            = log(nb_hap_pp[rank==1]) - log(nb_hap_pp[rank==2])),
+       by = .(chrom, start, end, sample, cell)]
+  probs <- probs[rank == 1]
+
+
+  # Clean up table
+  probs <- probs[, .(chrom, start, end, sample, cell, class, scalar, sv_call_name, sv_call_haplotype, sv_call_name_2nd, sv_call_haplotype_2nd, llr_to_ref, llr_to_2nd)]
+
+  return(probs[sv_call_name != "ref_hom" & llr_to_ref > llr_thr])
+}
+
+
 forceBiallelic <- function(probs, penalize_factor = 0.1)
 {
   # Add reference probability as an extra column (same as in makeSimpleSVCalls)
--- utils/mosaiClassifier_call.snakemake.R
+++ utils/mosaiClassifier_call.snakemake.R
@@ -4,7 +4,7 @@ source("utils/mosaiClassifier/makeSVcalls.R")
 probs = readRDS(snakemake@input[["probs"]])
 llr   = as.numeric(snakemake@wildcards[["llr"]])
 
-probs <- mosaiClassifierPostProcessing(probs)
-tab <- makeSVCallSimple(probs, llr_thr = llr)
+probs <- mosaiClassifierPostProcessing(probs, regularizationFactor=1e-6)
+tab <- makeSVCallWithPopPriors(probs, llr_thr = llr)
 
 write.table(tab, file = snakemake@output[[1]], sep = "\t", quote=F, row.names = F, col.names = T)
