3289991e238f3eec94948cfb297f584c1f9e834c
Maryam Ghareghani <mgharegh@d3compute02.mpi-inf.mpg.de>
2018-10-09 17:14:18 +0200

    added proper title and namings for the plots

--- utils/plotBreakpointLikelihoods.snakemake.R
+++ utils/plotBreakpointLikelihoods.snakemake.R
@@ -10,6 +10,7 @@ br.probs <- Reduce(rbind, lapply(snakemake@input[["breakpoint_ll"]], function(x)
 
 br.ll.hist <- ggplot(br.probs, aes(x=log(br_ll), fill=breakpoint_type!="no_br"))+geom_histogram(aes(y=..density..))
 br.ll.density <- ggplot(br.probs, aes(x=log(br_ll), col=breakpoint_type!="no_br"))+geom_density()
+l <- labs(x="log-likelihood of having the same SV", color="breakpoint", fill="breakpoint")
 
-ggsave(snakemake@output[["plot_breakpoint_ll"]], grid.arrange(br.ll.hist, br.ll.density, nrow=2,  ncol=1))
+ggsave(snakemake@output[["plot_breakpoint_ll"]], grid.arrange(br.ll.hist+l+ggtitle(paste("bin size =", snakemake@wildcards[["window_size"]]))+theme(plot.title=element_text(hjust=0.5)), br.ll.density+l, nrow=2,  ncol=1))
 
