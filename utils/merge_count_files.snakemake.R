55582a862231267a45dbb13a8788e3b2044dc28b
Maryam Ghareghani <mgharegh@d3compute09.mpi-inf.mpg.de>
2020-04-06 15:36:22 +0200

    added R script for merging segment counts

---
+++ utils/merge_count_files.snakemake.R
@@ -0,0 +1,27 @@
+log <- file(snakemake@log[[1]], open='wt')
+sink(file=log, type='message')
+sink(file=log, type='output')
+
+library(data.table)
+
+w <- snakemake@input[["w_counts"]]
+c <- snakemake@input[["c_counts"]]
+
+w.counts <- data.table()
+c.counts <- data.table()
+for (i in 1:length(w)){
+	cell.name <- gsub("manual_segments_", "", basename(w[[i]]))
+	cell.name <- gsub("_w_counts.txt", "", cell.name)
+	cell.w.counts <- fread(w[[i]], col.names=c('chrom', 'start', 'end', 'w'))
+	cell.w.counts[, cell:=cell.name]
+	w.counts <- rbind(w.counts, cell.w.counts)
+	cell.c.counts <- fread(c[[i]], col.names=c('chrom', 'start', 'end', 'c'))
+	cell.c.counts[, cell:=cell.name]
+	c.counts <- rbind(c.counts, cell.c.counts)
+}
+
+# TODO: add the class column later if needed
+counts <- merge(c.counts, w.counts, by=c("chrom","start","end","cell"))
+counts[, sample:=snakemake@wildcards[["sample"]]]
+
+fwrite(counts[, .(chrom,start,end,sample,cell,c,w)], snakemake@output[[1]], sep="\t", row.names=F)
